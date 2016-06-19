d429491f0a4b9688f53a008cbf0b863a44064a0b
addyyarci <adi.fuente3@gmail.com>
2016-06-18 20:40:57 -0500

    cmt_1806_0840

--- app/src/main/java/com/example/naird/nuevaactividad/Main2Activity.java
+++ app/src/main/java/com/example/naird/nuevaactividad/Main2Activity.java
@@ -1,13 +1,29 @@
 package com.example.naird.nuevaactividad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView texto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        texto = (TextView) findViewById(R.id.textoAct2);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        if(extras!=null){
+            String dato = extras.getString("DATO");
+            texto.setText(dato);
+        }
     }
 }
--- app/src/main/java/com/example/naird/nuevaactividad/MainActivity.java
+++ app/src/main/java/com/example/naird/nuevaactividad/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //comentando para git2
 
+    //habilitar los elementos
+
+    EditText editTextEnviar;
     Button boton;
 
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEnviar = (EditText) findViewById(R.id.editTextEnviar);
 
         boton = (Button) findViewById(R.id.button);
         boton.setOnClickListener(this);
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button:
+
+                String dato = editTextEnviar.getText().toString();
+
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                intent.putExtra("DATO", dato);
                 startActivity(intent);
                     break;
         }
